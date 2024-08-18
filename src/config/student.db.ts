497ed596615631a4f7cd38a2e02a22c60927660c
Tejasv Kumar <guptatejasv86086@gmail.com>
2024-08-18 14:44:27 +0530

    Fixed path

--- src/config/student.db.ts
+++ src/config/student.db.ts
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     mongoose
       .connect(
-        "mmongodb+srv://guptatejasv86086:IHiNa09sVQaCgM9N@cluster0.6etxq2u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        "mongodb+srv://guptatejasv86086:IHiNa09sVQaCgM9N@cluster0.6etxq2u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
       )
       .then(() => {
         console.log("Connected to the database.");
