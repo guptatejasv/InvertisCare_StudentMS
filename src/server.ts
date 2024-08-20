8adad8955cfccbd41bffaa2814607f08c34dc32d
Tejasv Kumar <guptatejasv86086@gmail.com>
2024-08-20 14:10:13 +0530

    add pug

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,30 @@ import router from "./routes/student.routes";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoSanitize from "express-mongo-sanitize";
+import path from "path";
 dotenv.config();
 connectDB();
 const port = process.env.PORT;
 
 const app = express();
+
+// Templates
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(mongoSanitize());
 app.use(express.urlencoded({ extended: true }));
-
+// Templates
+app.get("/", (req, res) => {
+  res.status(200).render("base");
+});
+app.get("/register", (req, res) => {
+  res.status(200).render("register");
+});
 app.use("/api/v1/", router);
 
 app.listen(port, () => {
